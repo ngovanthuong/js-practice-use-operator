c3f6b0fca5dd9e6a09f1b988addb0a5d83bb65f9
unknown <ngovanthuong>
2019-08-22 10:58:51 +0700

    commit

---
+++ baitap.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeight;
+inputHeight = prompt("nhap chieu cao");
+inputWidth = prompt(" nhap chieu rong ");
+let width = parseInt( inputWidth);
+let height = parseInt(inputHeight);
+let area = width*height;
+document.write("the area is = " + area);
